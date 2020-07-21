adcf8bed6bc84e145630883125d8a92688f57708
myfreeer <myfreeer@users.noreply.github.com>
2020-07-21 19:48:35 +0800

    detect-link-type: fix detecting resources from sitemap

--- src/mdn/process-url/detect-link-type.ts
+++ src/mdn/process-url/detect-link-type.ts
@@ -1,5 +1,5 @@
-import { arrayToMap } from 'website-scrap-engine/lib/util';
-import {ResourceType} from 'website-scrap-engine/lib/resource';
+import {arrayToMap} from 'website-scrap-engine/lib/util';
+import {Resource, ResourceType} from 'website-scrap-engine/lib/resource';
 
 // manually collected
 const validExtensionName = arrayToMap([
@@ -29,48 +29,48 @@ const validExtensionName = arrayToMap([
 export const detectLinkType = (
   url: string,
   type: ResourceType,
-  elem: Cheerio | null
+  elem: Cheerio | null,
+  parent: Resource | null
 ): ResourceType => {
-  if (!elem) {
+  if (!((elem && (elem.is('a') || elem.is('iframe'))) ||
+    (parent && parent.type === ResourceType.SiteMap))) {
     return type;
   }
-  if (elem.is('a') || elem.is('iframe')) {
-    if (url.includes('/@api/deki/files/')) {
-      return ResourceType.Binary;
-    }
-    if (url.includes('/docs/') ||
-      url.includes('Add-ons/WebExtensions') ||
-      url.includes('Add-ons/Firefox_for_Android') ||
-      url.includes('Apps/Build') ||
-      url.includes('JavaScript_code_modules/') ||
-      url.includes('Creating_XPCOM_Components/Building_the_WebLock_UI')) {
-      return ResourceType.Html;
-    }
+  if (url.includes('/@api/deki/files/') && !url.endsWith('.html')) {
+    return ResourceType.Binary;
+  }
+  if (url.includes('/docs/') ||
+    url.includes('Add-ons/WebExtensions') ||
+    url.includes('Add-ons/Firefox_for_Android') ||
+    url.includes('Apps/Build') ||
+    url.includes('JavaScript_code_modules/') ||
+    url.includes('Creating_XPCOM_Components/Building_the_WebLock_UI')) {
+    return ResourceType.Html;
+  }
 
-    const hashIndex: number = url.lastIndexOf('#');
-    const searchIndex: number = hashIndex === -1 ?
-      url.lastIndexOf('?') :
-      url.lastIndexOf('?', hashIndex);
-    const endIndex: number = searchIndex === -1 ?
-      hashIndex :
-      hashIndex === -1 ? searchIndex : Math.min(searchIndex, hashIndex);
-    const endPath: number = endIndex === -1 ?
-      url.lastIndexOf('/') :
-      url.lastIndexOf('/', endIndex);
-    const lastIndex: number = endIndex === -1 ?
-      url.lastIndexOf('.') :
-      url.lastIndexOf('.', endIndex);
-    if (lastIndex !== -1 && lastIndex > endPath) {
-      const extension: string = endIndex === -1 ?
-        url.slice(lastIndex + 1).toLowerCase() :
-        url.slice(lastIndex + 1, endIndex).toLowerCase();
-      if (validExtensionName[extension]) {
-        return ResourceType.Binary;
-      } else if ('css' === extension) {
-        return ResourceType.Css;
-      } else {
-        return ResourceType.Html;
-      }
+  const hashIndex: number = url.lastIndexOf('#');
+  const searchIndex: number = hashIndex === -1 ?
+    url.lastIndexOf('?') :
+    url.lastIndexOf('?', hashIndex);
+  const endIndex: number = searchIndex === -1 ?
+    hashIndex :
+    hashIndex === -1 ? searchIndex : Math.min(searchIndex, hashIndex);
+  const endPath: number = endIndex === -1 ?
+    url.lastIndexOf('/') :
+    url.lastIndexOf('/', endIndex);
+  const lastIndex: number = endIndex === -1 ?
+    url.lastIndexOf('.') :
+    url.lastIndexOf('.', endIndex);
+  if (lastIndex !== -1 && lastIndex > endPath) {
+    const extension: string = endIndex === -1 ?
+      url.slice(lastIndex + 1).toLowerCase() :
+      url.slice(lastIndex + 1, endIndex).toLowerCase();
+    if (validExtensionName[extension]) {
+      return ResourceType.Binary;
+    } else if ('css' === extension) {
+      return ResourceType.Css;
+    } else {
+      return ResourceType.Html;
     }
   }
   return type;
