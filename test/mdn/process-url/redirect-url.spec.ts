821493ec03d56d861f6688ce32e492ad00030928
myfreeer <myfreeer@users.noreply.github.com>
2020-07-26 11:15:27 +0800

    redirect-url: unit tests for issue #16
    https://github.com/myfreeer/mdn-local/issues/54

--- test/mdn/process-url/redirect-url.spec.ts
+++ test/mdn/process-url/redirect-url.spec.ts
@@ -26,10 +26,27 @@ describe('redirect-url', function () {
     // https://github.com/myfreeer/mdn-local/issues/16
     expect(redirectUrl('https://mdn.mozillademos.org/zh-CN/docs/Web/API/Element/innerHTML$samples/Example?revision=1522698',
       null, null, opt('zh-CN')))
-      .toBe('https://mdn.mozillademos.org/zh-CN/docs/Web/API/Element/innerHTML$samples/Example');
+      .toBe('https://developer.mozilla.org/zh-CN/docs/Web/API/Element/innerHTML$samples/Example');
     // locale redirected
     expect(redirectUrl('https://mdn.mozillademos.org/en-US/docs/Web/CSS/user-select$samples/Examples?revision=1613617',
       null, null, opt('zh-CN')))
-      .toBe('https://mdn.mozillademos.org/zh-CN/docs/Web/CSS/user-select$samples/Examples');
+      .toBe('https://developer.mozilla.org/zh-CN/docs/Web/CSS/user-select$samples/Examples');
+  });
+
+  // https://github.com/myfreeer/mdn-local/issues/16
+  test('discarding url search parameters #16', () => {
+    expect(redirectUrl('https://developer.mozilla.org/@api/deki/files/268/=MenuSystemCommands.png?size=webview',
+      null, null, opt('zh-CN')))
+      .toBe('https://developer.mozilla.org/@api/deki/files/268/=MenuSystemCommands.png');
+    expect(redirectUrl('https://developer.mozilla.org/zh-CN/docs/Web/API/IDBCursor?redirectlocale=en-US&redirectslug=IndexedDB%252FIDBCursor',
+      null, null, opt('zh-CN')))
+      .toBe('https://developer.mozilla.org/zh-CN/docs/Web/API/IDBCursor');
+    // this is not a regular redirect
+    expect(redirectUrl('https://developer.mozilla.org/zh-CN/Add-ons/Code_snippets/Tabbed_browser?redirectlocale=en-US&redirectslug=Code_snippets%252FTabbed_browser',
+      null, null, opt('zh-CN')))
+      .toBe('https://developer.mozilla.org/zh-CN/docs/Archive/Add-ons/Tabbed_browser');
+    expect(redirectUrl('https://developer.mozilla.org/En/docs/XUL/Attribute/align?raw&macros&include',
+      null, null, opt('zh-CN')))
+      .toBe('https://developer.mozilla.org/zh-CN/docs/XUL/Attribute/align');
   });
 });
