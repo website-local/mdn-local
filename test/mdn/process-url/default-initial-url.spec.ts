9abd3840b19ca3f05e533d1bff0bedade40aa4bc
myfreeer <myfreeer@users.noreply.github.com>
2020-08-05 21:10:02 +0800

    mdn: unit tests for default-initial-url
    Related:
    https://github.com/myfreeer/mdn-local/issues/6
    895b88350651dde02e6112438be5db2c14c2bd5e
    0bb6f570eca90c8e21c8f044cf66334a863aa856

---
+++ test/mdn/process-url/default-initial-url.spec.ts
@@ -0,0 +1,59 @@
+import {defaultInitialUrl} from '../../../src/mdn/process-url/default-initial-url';
+
+describe('default-initial-url', function () {
+  // related
+  // https://github.com/myfreeer/mdn-local/issues/6
+  // 895b88350651dde02e6112438be5db2c14c2bd5e
+  // 0bb6f570eca90c8e21c8f044cf66334a863aa856
+  test('en-US', () => {
+    expect(defaultInitialUrl('en-US')).toStrictEqual([
+      'https://developer.mozilla.org/en-US/docs/Web/API',
+      'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference',
+      'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+      'https://developer.mozilla.org/en-US/docs/Web/HTML/Index',
+      'https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes',
+      'https://developer.mozilla.org/en-US/docs/Web/HTML/Element',
+      'https://developer.mozilla.org/en-US/docs/Web/HTTP',
+      'https://developer.mozilla.org/en-US/docs/Web/Tutorials',
+      'https://developer.mozilla.org/en-US/docs/Web/Guide',
+      'https://developer.mozilla.org/en-US/docs/Web/Accessibility',
+      'https://developer.mozilla.org/en-US/docs/Web/Reference',
+      'https://developer.mozilla.org/en-US/docs/Web/Web_components',
+      'https://developer.mozilla.org/en-US/docs/Web/MathML',
+      'https://developer.mozilla.org/en-US/docs/Web',
+      'https://developer.mozilla.org/en-US/docs/Mozilla',
+      'https://developer.mozilla.org/en-US/docs/Mozilla/Tech',
+      'https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions',
+      'https://developer.mozilla.org/en-US/docs/Learn',
+      'https://developer.mozilla.org/en-US/docs/Games',
+      'https://developer.mozilla.org/en-US/docs/Glossary',
+      'https://developer.mozilla.org/sitemaps/en-US/sitemap.xml'
+    ]);
+  });
+  test('non-en-US', () => {
+    expect(defaultInitialUrl('zh-CN')).toStrictEqual([
+      'https://developer.mozilla.org/zh-CN/docs/Web/API',
+      'https://developer.mozilla.org/zh-CN/docs/Web/CSS/Reference',
+      'https://developer.mozilla.org/zh-CN/docs/Web/JavaScript',
+      'https://developer.mozilla.org/zh-CN/docs/Web/HTML/Index',
+      'https://developer.mozilla.org/zh-CN/docs/Web/HTML/Attributes',
+      'https://developer.mozilla.org/zh-CN/docs/Web/HTML/Element',
+      'https://developer.mozilla.org/zh-CN/docs/Web/HTTP',
+      'https://developer.mozilla.org/zh-CN/docs/Web/Tutorials',
+      'https://developer.mozilla.org/zh-CN/docs/Web/Guide',
+      'https://developer.mozilla.org/zh-CN/docs/Web/Accessibility',
+      'https://developer.mozilla.org/zh-CN/docs/Web/Reference',
+      'https://developer.mozilla.org/zh-CN/docs/Web/Web_components',
+      'https://developer.mozilla.org/zh-CN/docs/Web/MathML',
+      'https://developer.mozilla.org/zh-CN/docs/Web',
+      'https://developer.mozilla.org/zh-CN/docs/Mozilla',
+      'https://developer.mozilla.org/zh-CN/docs/Mozilla/Tech',
+      'https://developer.mozilla.org/zh-CN/docs/Mozilla/Add-ons/WebExtensions',
+      'https://developer.mozilla.org/zh-CN/docs/Learn',
+      'https://developer.mozilla.org/zh-CN/docs/Games',
+      'https://developer.mozilla.org/zh-CN/docs/Glossary',
+      'https://developer.mozilla.org/sitemaps/zh-CN/sitemap.xml',
+      'https://developer.mozilla.org/sitemaps/en-US/sitemap.xml'
+    ]);
+  });
+});
