7367d456bd68bcc4ee3d9637cbafc4bab0615a8e
myfreeer <myfreeer@users.noreply.github.com>
2021-01-02 17:04:12 +0800

    mdn: cleaning up process-html
    Close https://github.com/website-local/mdn-local/issues/195

--- src/mdn/process-html/index.ts
+++ src/mdn/process-html/index.ts
@@ -31,31 +31,6 @@ export const preProcessHtml = ($: CheerioStatic, html: Resource): CheerioStatic 
   }
   $('meta[name^="twitter"]').remove();
   $('meta[name^="og"]').remove();
-  $('.mdn-wiki-notice').remove();
-  // notice on top of page
-  $('.global-notice').remove();
-  // page footer
-  $('#nav-footer').remove();
-  $('.home-callouts').remove();
-  // newsletter box
-  $('.newsletter-box').remove();
-  $('.newsletter-container').remove();
-  // Hacks Blog
-  $('.column-hacks').remove();
-  // login link
-  $('#toolbox').remove();
-  // locale, edit, history
-  $('.document-actions').remove();
-  $('.dropdown-container').remove();
-  // search box
-  $('#nav-main-search').remove();
-  $('#edit-history-menu').remove();
-  $('.header-search').remove();
-  $('.contributors-sub').remove();
-  // script errors in this page
-  $('#kserrors').remove();
-  // login-related
-  $('#auth-modal').remove();
   // head link to alternate lang
   $('link[rel="alternate"]').remove();
   $('link[rel="preconnect"]').remove();
@@ -63,18 +38,25 @@ export const preProcessHtml = ($: CheerioStatic, html: Resource): CheerioStatic 
   $('link[rel="manifest"]').remove();
   $('link[rel="search"]').remove();
   $('link[rel="apple-touch-icon-precomposed"]').remove();
-  $('a[href$="$translate"]').remove();
-  $('a[href$="$edit"]').remove();
-  // no script mode ?
-  // $('script').remove();
-  // newsletter script
-  $('script[src*="newsletter"]').remove();
-  $('script[src*="auth-modal."]').remove();
-  // remove styles
-  $('link[rel="stylesheet"][href*="auth-modal."]').remove();
-  $('link[rel="stylesheet"][href*="subscriptions."]').remove();
-  $('link[rel="stylesheet"][href*="mdn-subscriptions."]').remove();
-  $('link[rel="stylesheet"][href*="banners."]').remove();
+  // notice on top of page
+  $('.global-notice').remove();
+  // page footer
+  $('#nav-footer').remove();
+  // newsletter box
+  $('.newsletter-box').remove();
+  $('.newsletter-container').remove();
+  // Hacks Blog
+  $('.column-hacks').remove();
+  // login link
+  $('#toolbox').remove();
+  // locale
+  $('.locale-container').remove();
+  // script errors in this page
+  $('#kserrors').remove();
+  // login-related - not needed
+  // $('.auth-container').remove();
+  // This is an archived page. It's not actively maintained.
+  $('.archived').remove();
   $('script[src*="perf."]').remove();
   // bcd-signal script, not needed for offline usage
   $('script[src*="react-bcd-signal"]').remove();
@@ -84,6 +66,20 @@ export const preProcessHtml = ($: CheerioStatic, html: Resource): CheerioStatic 
   // remove main script
   // /static/js/runtime-main.41503b2a.js
   $('script[src*="runtime-main."]').remove();
+  // react-main script, still on index page
+  $('script[src*="react-main."]').remove();
+  // newsletter script, on the index page
+  $('script[src*="newsletter"]').remove();
+  // login box script, on the index page
+  $('script[src*="auth-modal."]').remove();
+  // remove styles on the index page
+  $('link[rel="stylesheet"][href*="auth-modal."]').remove();
+  $('link[rel="stylesheet"][href*="home_newsletter."]').remove();
+  $('link[rel="stylesheet"][href*="subscriptions."]').remove();
+  $('link[rel="stylesheet"][href*="home_featured."]').remove();
+  $('link[rel="stylesheet"][href*="mdn-subscriptions."]').remove();
+  $('link[rel="stylesheet"][href*="banners."]').remove();
+
   let assetsData, text;
 
   $('script').each((index, el) => {
@@ -128,34 +124,15 @@ export const preProcessHtml = ($: CheerioStatic, html: Resource): CheerioStatic 
   $('.translationInProgress').remove();
   // translation
   $('#doc-pending-fallback').remove();
-  // $('iframe').remove();
-  $('iframe[src*="youtube.com/"]').remove();
-  // Tag pagination discarded
-  // https://github.com/myfreeer/mdn-local/issues/10
-  $('.pagination').remove();
-  // fix script
-  // language=HTML
-  $(`
-    <div style="display:none" class="script-workaround">
-      <div id="close-header-search"></div>
-      <div id="nav-main-search"></div>
-      <div id="main-q"></div>
-    </div>`).appendTo($('#main-header'));
   // We're converting our compatibility data into a machine-readable JSON format.
   preProcessRemoveCompatibilityTableWarning($);
   // Add icon to external links for new ui
   preProcessAddIconToExternalLinks($);
-  // keep href of red links by replacing class and fixup style
-  $('article a.new')
-    .removeClass('new')
-    .addClass('mdn-local-red-link');
-
   // remove google cdn stuff
   $('link[href*="googleapis.com"]').remove();
   $('script[src*="googleapis.com"]').remove();
   return $;
 };
-
 
 export const postProcessHtml = ($: CheerioStatic): CheerioStatic => {
   $('script').each((index, el) => {
