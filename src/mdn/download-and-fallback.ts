97b09f9de8bd493c528725d1ebac5e16a9cef728
myfreeer <myfreeer@users.noreply.github.com>
2021-01-17 12:25:34 +0800

    mdn: manually fallback 404 to en-US
    Close https://github.com/website-local/mdn-local/issues/196

---
+++ src/mdn/download-and-fallback.ts
@@ -0,0 +1,50 @@
+import type {HTTPError} from 'got';
+import {downloadResource} from 'website-scrap-engine/lib/life-cycle';
+import {Resource, ResourceType} from 'website-scrap-engine/lib/resource';
+import type {
+  DownloadResource,
+  RequestOptions
+} from 'website-scrap-engine/lib/life-cycle/types';
+import type {StaticDownloadOptions} from 'website-scrap-engine/lib/options';
+import {isUrlHttp} from 'website-scrap-engine/lib/util';
+import {notFound} from 'website-scrap-engine/lib/logger/logger';
+
+
+export async function downloadAndFallback(
+  res: Resource,
+  requestOptions: RequestOptions,
+  options: StaticDownloadOptions
+): Promise<DownloadResource | Resource | void> {
+  if (res.body) {
+    return res as DownloadResource;
+  }
+  if (res.type === ResourceType.StreamingBinary) {
+    return res;
+  }
+  if (!isUrlHttp(res.downloadLink)) {
+    return res;
+  }
+  const mdnLocalizedUrlPrefix =
+    `https://developer.mozilla.org/${options.meta.locale}/`;
+  // do nothing for en-US or non-mdn url
+  if (options.meta.locale as string === 'en-US' ||
+    !res.url.startsWith(mdnLocalizedUrlPrefix)) {
+    return downloadResource(res, requestOptions, options);
+  }
+  // not en-US
+  try {
+    return await downloadResource(res, requestOptions, options);
+  } catch (err) {
+    if (err && err.name === 'HTTPError' &&
+      (err as HTTPError)?.response?.statusCode === 404) {
+      notFound.warn('falling back localized 404 resource to en-US',
+        res.downloadLink, res.refUrl);
+      // fallback to en-US
+      res.downloadLink = 'https://developer.mozilla.org/en-US/' +
+        res.downloadLink.slice(mdnLocalizedUrlPrefix.length);
+      return await downloadResource(res, requestOptions, options);
+    } else {
+      throw err;
+    }
+  }
+}
--- src/mdn/life-cycle.ts
+++ src/mdn/life-cycle.ts
@@ -1,15 +1,18 @@
-import {defaultLifeCycle} from 'website-scrap-engine/lib/life-cycle';
+import {
+  defaultLifeCycle,
+  downloadResource
+} from 'website-scrap-engine/lib/life-cycle';
+import {
+  defaultDownloadOptions,
+  DownloadOptions
+} from 'website-scrap-engine/lib/options';
+import {processHtml} from 'website-scrap-engine/lib/life-cycle/adapters';
 import {skipProcess} from './process-url/skip-process';
 import {redirectUrl} from './process-url/redirect-url';
 import {detectLinkType} from './process-url/detect-link-type';
 import {redirectDownloadLink} from './process-url/redirect-download-link';
 import {dropResource} from './process-url/drop-resource';
 import {redirectUrlAfterFetch} from './process-url/redirect-url-after-fetch';
-import {
-  defaultDownloadOptions,
-  DownloadOptions
-} from 'website-scrap-engine/lib/options';
-import {processHtml} from 'website-scrap-engine/lib/life-cycle/adapters';
 import {postProcessHtml, preProcessHtml} from './process-html';
 import {
   postProcessInteractiveExample,
@@ -17,6 +20,7 @@ import {
 } from './process-html/process-interactive-examples';
 import {processYariSourceMap} from './process-source-maps';
 import {decompressSitemap} from './decompress-sitemap';
+import {downloadAndFallback} from './download-and-fallback';
 
 const lifeCycle = defaultLifeCycle();
 lifeCycle.linkRedirect.push(skipProcess, redirectUrl);
@@ -28,6 +32,11 @@ lifeCycle.processAfterDownload.unshift(
   processHtml(preProcessInteractiveExample),
   decompressSitemap
 );
+for (let i = 0; i < lifeCycle.download.length; i++) {
+  if (lifeCycle.download[i] === downloadResource) {
+    lifeCycle.download[i] = downloadAndFallback;
+  }
+}
 lifeCycle.processAfterDownload.push(
   processHtml(postProcessHtml),
   processHtml(postProcessInteractiveExample),
