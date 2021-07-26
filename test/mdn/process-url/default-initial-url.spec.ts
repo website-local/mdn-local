bd7f2e0d35548e2ee2b8aaee8927bb5ab0f4c46e
myfreeer <myfreeer@users.noreply.github.com>
2021-07-26 21:04:42 +0800

    mdn: fix a test

--- test/mdn/process-url/default-initial-url.spec.ts
+++ test/mdn/process-url/default-initial-url.spec.ts
@@ -28,7 +28,9 @@ describe('default-initial-url', function () {
       'https://developer.mozilla.org/en-US/docs/Games',
       'https://developer.mozilla.org/en-US/docs/Glossary',
       // https://github.com/website-local/mdn-local/issues/214
-      'https://developer.mozilla.org/sitemaps/en-US/sitemap.xml.gz'
+      'https://developer.mozilla.org/sitemaps/en-US/sitemap.xml.gz',
+      // https://github.com/website-local/mdn-local/issues/372
+      'https://developer.mozilla.org/en-US/search-index.json',
     ]);
   });
   test('non-en-US', () => {
@@ -55,7 +57,9 @@ describe('default-initial-url', function () {
       'https://developer.mozilla.org/zh-CN/docs/Glossary',
       // https://github.com/website-local/mdn-local/issues/214
       'https://developer.mozilla.org/sitemaps/zh-CN/sitemap.xml.gz',
-      'https://developer.mozilla.org/sitemaps/en-US/sitemap.xml.gz'
+      'https://developer.mozilla.org/zh-CN/search-index.json',
+      // https://github.com/website-local/mdn-local/issues/372
+      'https://developer.mozilla.org/sitemaps/en-US/sitemap.xml.gz',
     ]);
   });
 });
