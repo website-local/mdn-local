50e5ce4183f7a5b6e69edbd1347e21533efca280
myfreeer <myfreeer@users.noreply.github.com>
2025-03-23 13:08:13 +0800

    process: hidden interactive examples
    See https://github.com/mdn/yari/blob/v4.7.2/client/src/lit/play/controller.js#L36
    Part of https://github.com/website-local/mdn-local/issues/1142

--- src/mdn/process-html/process-new-interactive-examples.ts
+++ src/mdn/process-html/process-new-interactive-examples.ts
@@ -1,10 +1,14 @@
 import type {CheerioStatic} from 'website-scrap-engine/lib/types.js';
 
 const SELECTOR_HTML = '.code-example > .html.interactive-example > code,' +
-  ' .code-example > .html.interactive-example-choice > code';
+  ' .code-example > .html.interactive-example-choice > code,' +
+  ' .code-example > .html-hidden.interactive-example > code' +
+  ' .code-example > .html-hidden.interactive-example-choice > code,';
 
 const SELECTOR_CSS = '.code-example > .css.interactive-example > code,' +
-  ' .code-example > .css.interactive-example-choice > code';
+  ' .code-example > .css.interactive-example-choice > code,' +
+  ' .code-example > .css-hidden.interactive-example > code' +
+  ' .code-example > .css-hidden.interactive-example-choice > code,';
 
 export function preProcessInteractiveExample(
   $: CheerioStatic,
